3ce57a7218914462044c1ee018164eccc90e0957
Mr-Bilal-00 <mrbilalakram0@gmail.com>
2026-02-22 21:32:17 +0500

    english_dictionary.py

---
+++ p1_DIC.py
@@ -0,0 +1,10 @@
+DICT={"abandon":" چھوڑ دینا / ترک کرنا"
+,
+"brilliant":" ذہین / شاندار"
+,
+"courage":" ہمت / بہادری"
+,
+"delicate":" نازک / حساس"
+,
+"eager": " بے تاب / مشتاق"}
+print (DICT["eager"])
